a9fcbbb9ee2483856ca180dc7f0d6cd9780ba931
Patryk <pszindler@sii.pl>
2022-08-22 14:19:50 +0200

    initial commit

--- src/test/java/SiteTest.java
+++ src/test/java/SiteTest.java
@@ -1,18 +1,22 @@
+import io.github.bonigarcia.seljup.SeleniumJupiter;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.openqa.selenium.WebDriver;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class SiteTest extends BaseExtension {
+@ExtendWith(SeleniumJupiter.class)
+public class SiteTest {
 
     @ParameterizedTest
     @EnumSource(value = Titles.class, names = {"SII"})
     @DisplayName("Check the title of sii.pl")
     @Tag("regression")
     @Tag("booking")
-    void checkTitleForBookingCom(Titles title) {
+    void checkTitleForBookingCom(Titles title, WebDriver driver) {
         driver.get("https://sii.pl/");
         String actualTitle = driver.getTitle();
         assertThat(actualTitle).isEqualTo(title.toString());
@@ -23,7 +27,7 @@ public class SiteTest extends BaseExtension {
     @DisplayName("Check the title of onet.pl")
     @Tag("regression")
     @Tag("onet")
-    void checkTitleForOnetPl(Titles title) {
+    void checkTitleForOnetPl(Titles title, WebDriver driver) {
         driver.get("https://www.onet.pl");
         String actualTitle = driver.getTitle();
         assertThat(actualTitle).isEqualTo(title.toString());
@@ -34,7 +38,7 @@ public class SiteTest extends BaseExtension {
     @DisplayName("Check the title of kotuszkowo.pl")
     @Tag("regression")
     @Tag("kotuszkowo")
-    void checkTitleForKotuszkowoPl(Titles title) {
+    void checkTitleForKotuszkowoPl(Titles title, WebDriver driver) {
         driver.get("http://kotuszkowo.pl/");
         String actualTitle = driver.getTitle();
         assertThat(actualTitle).isEqualTo(title.toString());
@@ -45,7 +49,7 @@ public class SiteTest extends BaseExtension {
     @DisplayName("Check the title of filmweb.pl")
     @Tag("regression")
     @Tag("filmweb")
-    void checkTitleForFilmwebPl(Titles title) {
+    void checkTitleForFilmwebPl(Titles title, WebDriver driver) {
         driver.get("https://www.filmweb.pl");
         String actualTitle = driver.getTitle();
         assertThat(actualTitle).isEqualTo(title.toString());
@@ -57,7 +61,7 @@ public class SiteTest extends BaseExtension {
     @DisplayName("Check the title of selenium.dev")
     @Tag("regression")
     @Tag("selenium")
-    void checkTitleForSeleniumDev(Titles title) {
+    void checkTitleForSeleniumDev(Titles title, WebDriver driver) {
         driver.get("https://www.selenium.dev/documentation/en/webdriver/");
         String actualTitle = driver.getTitle();
         assertThat(actualTitle).isEqualTo(title.toString());
